227add4537cfe1e5888298b51d1d31ea39a2b78a
AliSoufi77 <96691885+AliSoufi77@users.noreply.github.com>
2022-04-22 19:46:33 +0400

    Manage Projects Page added

--- Driver.py
+++ Driver.py
@@ -22,6 +22,10 @@ def home():
 def about():
    return render_template("About.html")
 
+@app.route("/Manage")
+def manage():
+   return render_template("ManagePr.html")
+
 @app.route("/")
 def homedefault():
    return redirect(url_for('home'))
